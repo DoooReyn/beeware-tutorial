5af48b1a87a0fc27fe77181cf86158060236f034
DoooReyn <jl88744653@gmail.com>
2021-04-06 16:00:47 +0800

    add some widgets

--- helloworld/src/helloworld/app.py
+++ helloworld/src/helloworld/app.py
@@ -18,11 +18,25 @@ class HelloWorld(toga.App):
         """
         main_box = toga.Box(style=Pack(direction=COLUMN))
 
-        name_label = toga.Label("Your name:", style=Pack(padding=(0,5)))
+        name_label = toga.Label("Your name:", style=Pack(padding=(0, 5)))
+        name_input = toga.TextInput(style=Pack(flex=1))
+        name_box = toga.Box(style=(Pack(direction=ROW, padding=5)))
+        name_box.add(name_label)
+        name_box.add(name_input)
+        self.name_input = name_input
+
+        button = toga.Button(
+            "Say Hello!", on_press=self.say_hello, style=Pack(padding=5))
+
+        main_box.add(name_box)
+        main_box.add(button)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
+
+    def say_hello(self, widget):
+        print("Hello " + self.name_input.value)
 
 
 def main():
