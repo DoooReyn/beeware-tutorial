c94b4d999b45c26f6a6dde61d9c70eb7dbe59aaa
DoooReyn <jl88744653@gmail.com>
2021-04-07 17:39:01 +0800

    文件浏览选择对话框

--- bmfonttoolbox/src/bmfonttoolbox/app.py
+++ bmfonttoolbox/src/bmfonttoolbox/app.py
@@ -1,9 +1,14 @@
 """
 BMFont Toolbox
 """
+import os
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
+
+
+def get_default_path():
+    return os.path.abspath(os.path.join(os.environ.get("HOMEPATH"), "Pictures"))
 
 
 class BMFontToolbox(toga.App):
@@ -18,9 +23,42 @@ class BMFontToolbox(toga.App):
         """
         main_box = toga.Box()
 
+        textfield = toga.TextInput(
+            initial=get_default_path(),
+            placeholder="Choose pictures folder",
+            readonly=True,
+            style=Pack(padding=(4, 4, 0, 4), flex=1)
+        )
+        button = toga.Button(
+            "Choose",
+            style=Pack(padding=(4, 4, 4, 0)),
+            on_press=self.on_select_texture_dir
+        )
+        box = toga.Box(style=Pack(direction=ROW))
+        box.add(textfield)
+        box.add(button)
+
+        main_box.add(box)
+
+        self.textfield = textfield
+        self.folder_button = button
+
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
+
+    def on_select_texture_dir(self, widget):
+        try:
+            where = get_default_path()
+            folder = self.main_window.select_folder_dialog(
+                title="Choose pictures folder",
+                initial_directory=where,
+                multiselect=False)
+            if folder and len(folder) > 0:
+                self.textfield.value = folder[0]
+            print(folder)
+        except ValueError as e:
+            print(e)
 
 
 def main():
