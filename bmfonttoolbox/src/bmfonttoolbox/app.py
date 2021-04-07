5847ff472d9dc20c908626c89a4f77e17e8e20cc
DoooReyn <jl88744653@gmail.com>
2021-04-07 18:13:45 +0800

    添加输出目录文件选择对话框

--- bmfonttoolbox/src/bmfonttoolbox/app.py
+++ bmfonttoolbox/src/bmfonttoolbox/app.py
@@ -11,6 +11,10 @@ def get_default_path():
     return os.path.abspath(os.path.join(os.environ.get("HOMEPATH"), "Pictures"))
 
 
+def get_output_path():
+    return os.path.abspath(os.path.join(os.environ.get("HOMEPATH"), "Documents"))
+
+
 class BMFontToolbox(toga.App):
 
     def startup(self):
@@ -21,31 +25,64 @@ class BMFontToolbox(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box()
+        self.setup_ui()
 
-        textfield = toga.TextInput(
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = self.main_box
+        self.main_window.show()
+
+    def setup_ui(self):
+        self.main_box = toga.Box(style=Pack(direction=COLUMN))
+        self.create_original_box()
+        self.create_output_box()
+
+    def create_original_box(self):
+        label = toga.Label(
+            "Choose picture folder",
+            style=Pack(padding=(4, 4, 0, 4))
+        )
+
+        self.original_textfield = toga.TextInput(
             initial=get_default_path(),
-            placeholder="Choose pictures folder",
             readonly=True,
             style=Pack(padding=(4, 4, 0, 4), flex=1)
         )
-        button = toga.Button(
+
+        self.original_button = toga.Button(
             "Choose",
             style=Pack(padding=(4, 4, 4, 0)),
             on_press=self.on_select_texture_dir
         )
+
         box = toga.Box(style=Pack(direction=ROW))
-        box.add(textfield)
-        box.add(button)
+        box.add(label)
+        box.add(self.original_textfield)
+        box.add(self.original_button)
+        self.main_box.add(box)
 
-        main_box.add(box)
+    def create_output_box(self):
+        label = toga.Label(
+            "Choose output folder",
+            style=Pack(padding=(4, 4, 0, 4))
+        )
 
-        self.textfield = textfield
-        self.folder_button = button
+        self.output_textfield = toga.TextInput(
+            initial=get_output_path(),
+            readonly=True,
+            style=Pack(padding=(4, 4, 0, 4), flex=1)
+        )
 
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
-        self.main_window.show()
+        self.output_button = toga.Button(
+            "Choose",
+            style=Pack(padding=(4, 4, 4, 0)),
+            on_press=self.on_select_texture_dir
+        )
+
+        box = toga.Box(style=Pack(direction=ROW))
+        box.add(label)
+        box.add(self.output_textfield)
+        box.add(self.output_button)
+        self.main_box.add(box)
 
     def on_select_texture_dir(self, widget):
         try:
@@ -53,12 +90,12 @@ class BMFontToolbox(toga.App):
             folder = self.main_window.select_folder_dialog(
                 title="Choose pictures folder",
                 initial_directory=where,
-                multiselect=False)
+                multiselect=False
+            )
             if folder and len(folder) > 0:
-                self.textfield.value = folder[0]
-            print(folder)
-        except ValueError as e:
-            print(e)
+                self.original_textfield.value = folder[0]
+        except ValueError:
+            pass
 
 
 def main():
